09e514533dcf20e030770e7bd37bfe613caecd6b
Reznov <84386112+Reznov57@users.noreply.github.com>
2021-08-11 20:47:20 +0400

    Class 11.08.21

---
+++ test1/test.js
@@ -0,0 +1,38 @@
+//let number = prompt("Helloy world", 0);
+
+//alert(number*number);
+
+/*let number = prompt("Введите первое число", 0);
+let first = prompt("Введите второе число", 0);
+
+let sum = Number(first)+ Number(number)
+
+alert(sum/2);*/
+
+/*let number = prompt("Введите сторону квадрата", 0);
+
+alert(number*number)*/
+
+/*let number = prompt("Введите км", 0);
+const mile = 0.621371;
+alert(number*mile)*/
+
+/*let number = prompt("Введите первое число", 0);
+let first = prompt("Введите второе число", 0);
+
+let sum = Number(first)+ Number(number)
+let multiplication = Number(first)* Number(number)
+let substraction = Number(first)- Number(number)
+let division = Number(first)/ Number(number)
+
+alert(`сумма ${sum}, умножение ${multiplication}, вычитание ${substraction}, деление ${division}`);*/
+
+let numbera = prompt ("Введите переменную a", 0);
+let numberb = prompt ("Введите переменную b", 0);
+
+let numberx = -Number(numberb)/Number(numbera);
+
+
+alert(numberx);
+
+
